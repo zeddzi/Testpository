cd7f19ddee60152bc6fc3d01c4633b0c5e1d1a90
zeddzi <i.am.mbuguaa34@gmail.com>
2022-07-10 15:09:57 +0300

    first commit

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("navLinks")
+function showMenu(argument) {
+    navLinks.style.right ="0"
+}
+function hideMenu(argument) {
+    navLinks.style.right ="-200px"
+}
